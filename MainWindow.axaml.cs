7a30c13fa56c42e6dad7e88f00c8c8b17e3958d3
Timardo <33033989+Timardo@users.noreply.github.com>
2024-11-13 15:33:42 +0100

    Init

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Snowman
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
