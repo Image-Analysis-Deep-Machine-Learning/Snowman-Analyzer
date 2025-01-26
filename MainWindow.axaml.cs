078bfecbecc800885312ee4a7988b9d87b2ead67
Timardo <33033989+Timardo@users.noreply.github.com>
2025-01-26 12:24:09 +0100

    Add simple viewport and menu

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,19 +1,25 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Interactivity;
+using Snowman.Controls;
+using Snowman.Core;
+using Snowman.DataContexts;
 
 namespace Snowman
 {
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Core of the app containing all data and providing methods for their manipulation from the main GUI
+        /// </summary>
+        public SnowmanApp CoreApp { get; }
+
         public MainWindow()
         {
+            DataContext = this;
+            CoreApp = new SnowmanApp();
             InitializeComponent();
-        }
-
-        private void Button_Click_2(object? sender, RoutedEventArgs e)
-        {
-            Close();
         }
     }
 }
